a8295c7cdd8c17556343de5bfa3c98bd9033c400
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2016-05-25 17:52:14 +0300

    #3 issue 'Admin Yetkileri' bug is fixed.

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,6 +1,11 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation, :user_name,
-                admin_user_group_attributes: [:id, :_destroy, :group_id]
+  permit_params do
+    params = [:email, :password, :password_confirmation, :user_name]
+    params.push admin_user_group_attributes: [:id, :_destroy, :group_id]
+    params.push :is_super if current_admin_user.is_super?
+    params
+  end
+
 
   index do
     selectable_column
